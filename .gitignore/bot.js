0079baf66e6db2fa972978afe93f87fed10eaa4e
Ixonoii <54681124+Ixonoii@users.noreply.github.com>
2019-12-25 18:23:05 +0100

    Update bot.js

--- .gitignore/bot.js
+++ .gitignore/bot.js
@@ -3,9 +3,10 @@
 const Discord = require("discord.js");
 const client = new Discord.Client;
 const fs = require('fs');
-var color = "#4d78f0";
+var color = "servembed";
 var activity = "-aide | -invite";
 var prefix = "-";
+var credits = "Arplex - Tous droits réservés"
 
 client.login(process.env.BOT_TOKEN)
 
@@ -22,12 +23,15 @@ function emoji (id) {
 client.on('message', message =>{
     if(message.content === prefix + "serveur"){
         let servembed = new Discord.RichEmbed()
-        .setTitle("Voici quelques informations à propos du serveur " + message.guild.name)
+        .setTitle("__Voici quelques informations à propos du serveur " + message.guild.name + "__")
         .setThumbnail(message.guild.iconURL)
-        .addField("Propriétaire du serveur :", message.guild.owner.displayName)
-        .addField("Nom du serveur :", message.guild.name, true)
-        .addField("ID du serveur :", message.guild.id, true)
-        .addField("Nombre de membres :", message.guild.memberCount + " membres", true)
+        .setColor(color)
+        .addField("Propriétaire du serveur", message.guild.owner.displayName)
+        .addField("Nom du serveur", message.guild.name, true)
+        .addField("ID du serveur", message.guild.id, true)
+        .addField("Nombre de membres", message.guild.memberCount + " membres", true)
+        .addField("Date de création du serveur", message.guild.createdAt)
+        .setTimestamp(Date.now)
         message.channel.send(servembed)
     }
 })
