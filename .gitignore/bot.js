d533082365bf12b0df8ddfa108b60a62f49c61fd
Ixonoii <54681124+Ixonoii@users.noreply.github.com>
2020-01-10 17:12:51 +0100

    Update bot.js

--- .gitignore/bot.js
+++ .gitignore/bot.js
@@ -1,13 +1,19 @@
-// CONFIGURATION //
+// ----------------------------------------- SETTINGS ----------------------------------------- //
 
 const Discord = require("discord.js");
 const client = new Discord.Client;
 const fs = require('fs');
-var color = "#4d78f0";
-var prefix = "-";
+var successcolor = "#04f028";
+var errorcolor = "#ff0000";
+var prefix = ";";
+var status = prefix + "aide";
 
 client.login(process.env.BOT_TOKEN)
 
 function emoji (id) {
     return client.emojis.get(id).toString();
 }
+
+client.on('ready', function(){
+    client.user.setActivity(status, {type: "PLAYING"})
+})
